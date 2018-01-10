11f3e4e38fedaf75f90cfe51d022790e66401b7c
wkurylo88 <wkurylo88@gmail.com>
2018-01-10 10:12:49 +0100

    * implemented SunshineSyncTask ** getURL; ** HTTP request; ** get JSON; ** get ContentValues[]; ** delete SQLite Table content; ** insert to SQLite via ContentResolver -> CProvider / method: bulkInsert

--- app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
+++ app/src/main/java/com/example/android/sunshine/sync/SunshineSyncTask.java
@@ -1,4 +1,63 @@
-//  TODO (1) Create a class called SunshineSyncTask
-//  TODO (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
-//      TODO (3) Within syncWeather, fetch new weather data
-//      TODO (4) If we have valid results, delete the old data and insert the new
+package com.example.android.sunshine.sync;
+
+//  COMPLETED (1) Create a class called SunshineSyncTask
+
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.content.Context;
+import android.net.Uri;
+import android.util.Log;
+
+import com.example.android.sunshine.data.WeatherContract;
+import com.example.android.sunshine.utilities.NetworkUtils;
+import com.example.android.sunshine.utilities.OpenWeatherJsonUtils;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class SunshineSyncTask{
+
+    private String TAG = SunshineSyncTask.this.getClass().getSimpleName();
+
+    //  COMPLETED (2) Within SunshineSyncTask, create a synchronized public static void method called syncWeather
+    public synchronized void syncWeather(Context context){
+
+
+        //      COMPLETED (3) Within syncWeather, fetch new weather data
+
+        URL weatherUrl = NetworkUtils.getUrl(context);
+        if(weatherUrl == null) return;
+
+        String jsonResponse = null;
+        try{
+            jsonResponse = NetworkUtils.getResponseFromHttpUrl(weatherUrl);
+        }catch (IOException e){
+            Log.e(TAG, "Error in JSON parser");
+        }
+
+        ContentValues[] weatherContentValues = null;
+        try{
+            weatherContentValues = OpenWeatherJsonUtils.getWeatherContentValuesFromJson(context,jsonResponse);
+        }catch (JSONException e){
+            Log.e(TAG, "Error in creating ContentValues[]");
+        }
+
+        //      COMPLETED (4) If we have valid results, delete the old data and insert the new
+        Uri contentProviderUri = WeatherContract.WeatherEntry.CONTENT_URI;
+        if (weatherContentValues != null && weatherContentValues.length != 0) {
+
+            ContentResolver sunshineContentResolver = context.getContentResolver();
+
+            int deletedRows = sunshineContentResolver.delete(contentProviderUri,null,null);
+            int insertedRows = sunshineContentResolver.bulkInsert(contentProviderUri,weatherContentValues);
+
+        }
+
+    }
+
+
+
+
+}
